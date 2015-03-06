7cecc9017e6ab3d6beb6a8e6f9ccd94ff65e60ab
Lee Sheng <bogosort@gmail.com>
2015-03-05 23:21:06 -0800

    Add base Fibonacci

---
+++ fibonacci/Fibonacci.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+class Fibonacci {
+  public static int fib(int n) {
+    /** write code here. */
+    return 0;
+  }
+
+  public static void main(String[] args) {
+    System.out.println("fib(0) = " + fib(0));
+    System.out.println("fib(1) = " + fib(1));
+    System.out.println("fib(2) = " + fib(2));
+  }
+}
+
---
+++ fibonacci/Fibonacci.md
@@ -0,0 +1,1 @@
+Write a method that returns the nth Fibonacci number with the seed of fib(0)=0 and fib(1)=1.
