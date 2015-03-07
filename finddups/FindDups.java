f52dd4882d87182272dc679c74cce1509e35a4cc
Lee Sheng <bogosort@gmail.com>
2015-03-06 23:50:09 -0800

    add finddups

---
+++ finddups/FindDups.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+class FindDups {
+  public static Boolean findDups(int... numbers) {
+    /** write code here. */
+    return true;
+  }
+
+  public static Boolean findDupsWithDistance(int k, int... numbers) {
+    /** write code here. */
+    return true;
+  }
+
+  public static Boolean findDupsWithTolerance(int k, int l, int... numbers) {
+    /** write code here. */
+    return true;
+  }
+
+  public static void main(String[] args) {
+    System.out.println("findDups(1,2,1) = " + findDups(1,2,1));
+  }
+}
+
---
+++ finddups/FindDups.md
@@ -0,0 +1,4 @@
+Write 3 methods:
+tier 1: return whether there are duplicates in a list of integers (O(n))
+tier 2: return whether there are duplicates within k distance away from each other in a list (if they are more than k indices away, they don't count as duplicates) (O(n) still)
+tier 3: return whether there are duplicates within k distance of each other, where 2 numbers are duplicates if they are within a tolerance "l" (lower case L) of each other (O(n) optimal, O(n*k) ok)
